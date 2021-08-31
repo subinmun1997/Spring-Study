a424ad613e2cdd3055427664847baef58589e7e8
subinmun1997 <qzxy812@gmail.com>
2021-08-31 17:02:42 +0900

    Add HelloController

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,7 +1,15 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("hello") // 웹 어플리케이션에서 /hello라고 들어오면 이 메서드 호출
+    public String hello(Model model) {
+        model.addAttribute("data","spring!");
+        return "hello"; // templates/hello.html를 찾아서 화면을 실행시켜라
+    }
 }
