522406b982339b75f475eb823d4cc3d9e34e36e1
subinmun1997 <qzxy812@gmail.com>
2021-09-03 14:57:52 +0900

    Add 홈 화면 추가, 회원 등록, 회원 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -25,5 +31,28 @@ public class MemberController {
      */
     public MemberController(MemberService memberService) { // 생성자 주입
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new") // url창에다 치는건 GetMapping (조회할 때 주로)
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new") // 데이터를 form에 넣어서 전달할 때 PostMapping (데이터 등록할 때 주로)
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        //System.out.println("member = " + member.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
